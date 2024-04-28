df4814d9611875087873ba7170341ac45efacd62
Hishllyi <2962253473@qq.com>
2024-04-28 16:43:23 +0800

    修改rename无法读取图像路径的bug

--- RenameFilesWithSpecificSuffix/rename.py
+++ RenameFilesWithSpecificSuffix/rename.py
@@ -10,19 +10,18 @@ def rename_files(img_folder, file_ext, new_name_prefix):
     @param new_name_prefix: 改名的文件前缀
     @return:
     """
-    images = glob.glob(f"{img_folder}\\*{file_ext}")
+    images = glob.glob(f"{img_folder}/*{file_ext}")
 
     for idx, img_path in enumerate(images):
         img_name, img_ext = os.path.splitext(os.path.basename(img_path))
         new_img_name = f"{new_name_prefix}{idx + 1}{img_ext}"
         new_img_path = os.path.join(img_folder, new_img_name)
         os.rename(img_path, new_img_path)
-
         print(f"Renamed {img_path} to {new_img_path}")
 
 
 if __name__ == '__main__':
-    img_folder = "CameraCalibration\\images\\datasets"  # 指定修改的目录
+    img_folder = "../CameraCalibration/images/datasets"  # 指定修改的目录
     file_ext = ".jpg"  # 指定文件后缀名
     new_name_prefix = "image00"
 
